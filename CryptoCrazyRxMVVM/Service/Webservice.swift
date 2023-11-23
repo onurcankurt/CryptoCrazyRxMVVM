99d524827fcabf478e571f40cbadd34005d69c2e
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-23 17:47:53 +0300

    webservice codes added

--- CryptoCrazyRxMVVM/Service/Webservice.swift
+++ CryptoCrazyRxMVVM/Service/Webservice.swift
@@ -17,6 +17,17 @@ class Webservice {
     func downloadCurrencies(url: URL, completion: @escaping (Result<[Crypto], CryptoError>) -> ()){
         URLSession.shared.dataTask(with: url) { data, response, error in
             
-        }
+            if let _ = error {
+                completion(.failure(.serverError))
+            } else if let data = data {
+                let cryptoList = try? JSONDecoder().decode([Crypto].self, from: data)
+                if let cryptoList = cryptoList {
+                    completion(.success(cryptoList))
+                } else {
+                    completion(.failure(.parsingError))
+                }
+            }
+            
+        }.resume()
     }
 }
