42ff75c348bc2a5982da0580de92d86ea98e246b
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-23 23:57:18 +0300

    bindings and activity indicator added

--- CryptoCrazyRxMVVM/View/ViewController.swift
+++ CryptoCrazyRxMVVM/View/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    
     @IBOutlet weak var tableView: UITableView!
+    
     let cryptoVM = CryptoViewModel()
     let disposeBag = DisposeBag()
     
@@ -20,15 +23,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.delegate = self
-        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.dataSource = self
         
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
         setupBindings()
         cryptoVM.requestData()
         
     }
     
     private func setupBindings(){
+        
+        cryptoVM
+            .loading
+            .bind(to: self.indicatorView.rx.isAnimating)
+            .disposed(by: disposeBag)
         
         cryptoVM
             .error
@@ -38,27 +47,36 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
             .disposed(by: disposeBag)
         
+//        cryptoVM
+//            .cryptos
+//            .observe(on: MainScheduler.asyncInstance)
+//            .subscribe { cryptos in
+//                self.cryptoList = cryptos
+//                self.tableView.reloadData()
+//            }.disposed(by: disposeBag)
+        
         cryptoVM
             .cryptos
             .observe(on: MainScheduler.asyncInstance)
-            .subscribe { cryptos in
-                self.cryptoList = cryptos
-                self.tableView.reloadData()
-            }.disposed(by: disposeBag)
+            .bind(to: tableView.rx.items(cellIdentifier: "CryptoCell", cellType: CryptoTableViewCell.self)){row, item, cell in
+                cell.item = item
+            }
+            .disposed(by: disposeBag)
+        
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cryptoList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        var content = cell.defaultContentConfiguration()
-        content.text = cryptoList[indexPath.row].currency
-        content.secondaryText = cryptoList[indexPath.row].price
-        cell.contentConfiguration = content
-        return cell
-    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return cryptoList.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        var content = cell.defaultContentConfiguration()
+//        content.text = cryptoList[indexPath.row].currency
+//        content.secondaryText = cryptoList[indexPath.row].price
+//        cell.contentConfiguration = content
+//        return cell
+//    }
 
 }
 
